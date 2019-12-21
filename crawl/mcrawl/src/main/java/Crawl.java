e614fc5b4b3fd8ed998f05482426802f2b832974
Sandmannn <bohdanpukalskyi@gmail.com>
2019-12-21 23:03:37 +0100

    older crawler files

--- crawl/mcrawl/src/main/java/Crawl.java
+++ crawl/mcrawl/src/main/java/Crawl.java
@@ -10,9 +10,8 @@ import java.net.*;
 import java.util.List;
 
 public class Crawl {
-    public static void main(String[] args) throws Exception {
-        System.out.println("mew");
 
+    static List<String> getCrudeContent() throws Exception {
         CookieHandler.setDefault(new CookieManager(null, CookiePolicy.ACCEPT_ALL));
 
         URL url= new URL(Config.urlAddress);
@@ -33,8 +32,22 @@ public class Crawl {
         in.close();
         Document doc = Jsoup.parseBodyFragment(sb.toString());
         List<String> titles = doc.select("[title^=Details]").eachText();
+        return titles
+    }
+
+    static void getImdbValues(String descr) {
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("mew");
+        List<String> titles = getCrudeContent();
         System.out.println(titles);
 
+
+//"... denn sie wissen nicht, was sie tun / Regie: Nicholas Ray. Darst.: James Dean ; Natalie Wood ; Sal Mineo ... (Focus Edition ; 19) - 1955, USA"
+//   8 1/2 / Regie und Drehb.: Federico Fellini. Darst.: Marcello Mastroianni ; Claudia Cardinale ; Anouk Aim�e... (Arthaus Close-Up) - 1961, Italien
+// Alfred Hitchcock Collector's Edition : Gute Reise! ; Landung auf Madagaskar ; Nummer 17 ; Sabotage ; Der Weltmeister / Regie: Alfred Hitchcock ... - 1927-1944, GB
 
     }
 }
