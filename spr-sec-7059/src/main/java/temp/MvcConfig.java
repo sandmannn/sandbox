cb784860e0d7c1dd1468de0db4b7fbb6ff43c8b7
Sandmannn <bohdanpukalskyi@gmail.com>
2019-12-30 17:01:25 +0100

    added unsecured view for admin

--- spr-sec-7059/src/main/java/temp/MvcConfig.java
+++ spr-sec-7059/src/main/java/temp/MvcConfig.java
@@ -12,6 +12,7 @@ public class MvcConfig implements WebMvcConfigurer {
     registry.addViewController("/home").setViewName("home");
     registry.addViewController("/").setViewName("home");
     registry.addViewController("/hello").setViewName("hello");
+    registry.addViewController("/admin_only").setViewName("admin_only");
     registry.addViewController("/login").setViewName("login");
   }
 
