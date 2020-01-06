e784ff764b1eb00bde6effb501cb96c1cc162460
Sandmannn <bohdanpukalskyi@gmail.com>
2020-01-06 18:07:03 +0100

    migrated from tymea to jsp, security off

--- spr-sec-7059/src/main/java/temp/MvcConfig.java
+++ spr-sec-7059/src/main/java/temp/MvcConfig.java
@@ -1,21 +1,35 @@
 
 package hello;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-  public void addViewControllers(ViewControllerRegistry registry) {
-    registry.addViewController("/home").setViewName("home");
-    registry.addViewController("/").setViewName("home");
-    registry.addViewController("/hello").setViewName("hello");
-    registry.addViewController("/admin_only").setViewName("admin_only");
-    registry.addViewController("/user_also").setViewName("user_also");
-    registry.addViewController("/login").setViewName("login");
+
+  @Bean
+  public ViewResolver jspViewResolver() {
+    InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+    viewResolver.setPrefix("/WEB-INF/jsp/");
+    viewResolver.setSuffix(".jsp");
+    return viewResolver;
   }
+
+
+//  public void addViewControllers(ViewControllerRegistry registry) {
+//    registry.addViewController("/home").setViewName("home");
+//    registry.addViewController("/").setViewName("home");
+//    registry.addViewController("/hello").setViewName("hello");
+//    registry.addViewController("/welcome").setViewName("welcome");
+//    registry.addViewController("/admin_only").setViewName("admin_only");
+//    registry.addViewController("/user_also").setViewName("user_also");
+//    registry.addViewController("/login").setViewName("login");
+//  }
 
 }
 
