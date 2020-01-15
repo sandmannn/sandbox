6a7d90c970514d0c18e61c333d7d7e876027cd54
Sandmannn <bohdanpukalskyi@gmail.com>
2020-01-15 22:54:01 +0100

    added h2 dependency

--- spr-sec-acl-7598/src/main/java/temp/Application.java
+++ spr-sec-acl-7598/src/main/java/temp/Application.java
@@ -4,10 +4,21 @@
 
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.annotation.PostConstruct;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
@@ -15,6 +26,40 @@ public class Application extends SpringBootServletInitializer {
   public static void main(String[] args) throws Throwable {
     SpringApplication.run(Application.class, args);
   }
+
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
+
+  @PostConstruct
+  private void initDb() {
+    System.out.println(String.format("****** Creating table: %s, and Inserting test data ******", "Employees"));
+
+    String sqlStatements[] = {
+            "drop table employees if exists",
+            "create table employees(id serial,first_name varchar(255),last_name varchar(255))",
+            "insert into employees(first_name, last_name) values('Eugen','Paraschiv')",
+            "insert into employees(first_name, last_name) values('Scott','Tiger')"
+    };
+
+    Arrays.asList(sqlStatements).stream().forEach(sql -> {
+      System.out.println(sql);
+      jdbcTemplate.execute(sql);
+    });
+
+    System.out.println(String.format("****** Fetching from table: %s ******", "Employees"));
+    jdbcTemplate.query("select id,first_name,last_name from employees",
+            new RowMapper<Object>() {
+              @Override
+              public Object mapRow(ResultSet rs, int i) throws SQLException {
+                System.out.println(String.format("id:%s,first_name:%s,last_name:%s",
+                        rs.getString("id"),
+                        rs.getString("first_name"),
+                        rs.getString("last_name")));
+                return null;
+              }
+            });
+  }
+
 
   @Override
   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
